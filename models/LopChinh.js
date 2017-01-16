6fd18f4c31633c982f88abca6d4c224feed4bcc2
duckhanhuet <duckhanhk59uet.com>
2017-01-16 09:37:40 +0700

    Initial commit

---
+++ models/LopChinh.js
@@ -0,0 +1,15 @@
+var mongoose    = require('mongoose');
+var Schema  = mongoose.Schema;
+var LopChinhSchema = mongoose.Schema({
+    tenLopChinh:{
+        type: String
+    },
+    idKhoa:{
+        type: Schema.Types.ObjectId,
+        ref :'Khoa'
+    }
+});
+
+
+
+module.exports = mongoose.model('LopChinh',LopChinhSchema);
